1fa8721ef0cc297283b0678e80249025f1881815
Daniel <danielgondimdecastro@gmail.com>
2023-10-03 20:20:16 -0300

    feat: commit inicial do projeto SpringBoot para aula de Ferramenta de Desenvolvimento de Software

---
+++ atividadeRoussian/src/main/java/com/ads/atividadeRoussian/AtividadeRoussianApplication.java
@@ -0,0 +1,13 @@
+package com.ads.atividadeRoussian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeRoussianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeRoussianApplication.class, args);
+	}
+
+}
---
+++ atividadeRoussian/src/test/java/com/ads/atividadeRoussian/AtividadeRoussianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.atividadeRoussian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeRoussianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
